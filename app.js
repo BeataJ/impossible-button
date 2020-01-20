1f0cc82e854764787fb538a6bc8d69436d2c51ec
beata <beata@emc2wave.com>
2020-01-20 14:58:43 -0800

    add btn.style.left, btn.style.top

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const btn = document.querySelector("button");
 
 btn.addEventListener("mouseover", function() {
   console.log("MOUSED OVER ME!");
+  btn.style.left = "200px";
+  btn.style.top = "100px";
 });
